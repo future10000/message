746172324a1f07fabbe235eeca24e173234e43d7
panjing <pjing02468@126.com>
2020-04-25 15:09:41 +0800

    添加model

---
+++ model/Base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PJ
+ * Date: 2020/4/25
+ * Time: 15:07
+ */
+namespace app\model;
+
+use yii\db\ActiveRecord;
+
+class Base extends ActiveRecord
+{
+    
+}
